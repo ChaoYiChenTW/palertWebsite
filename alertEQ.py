e98dc30b52b2002562d2052d6fab10a15076c232
ChaoYi <chaoyi@earth.sinica.edu.tw>
2021-10-21 10:37:42 +0800

    clean up code

--- alertEQ.py
+++ alertEQ.py
@@ -3,58 +3,103 @@
 #
 # to monitor the CWB opendata platform for latest earthquake report
 
-from library import unzipFile, getOriginTimeUTC, getEqParameters, getCwbStationInfo
+import library
 import os
 import pandas as pd
 import xml.etree.ElementTree as ET
 import wget
 
-cwbOpendataApi = "opendataapi?dataid=E-A0015-001&authorizationkey=CWB-BFBE0988-4DB7-47A8-AE56-3D82EFBFDF6E"
 
 def main():
     xmlDir = 'xml'
     os.chdir(xmlDir)
-    
-    removeExistOpendataApi()
-    # download data from cwb opendata api
-    downloadZipfile = "download.zip"
-    downloadXMLfiles(downloadZipfile)
+
+    cwbOpendataApi = "opendataapi?dataid=E-A0015-001&authorizationkey=CWB-BFBE0988-4DB7-47A8-AE56-3D82EFBFDF6E"
+    library.removeFile(cwbOpendataApi)
+    zipfile = "download.zip"
+    downloadXMLfiles(cwbOpendataApi, zipfile)
 
     xmlFolder = 'xmlfiles'
-    unzipFile(downloadZipfile, xmlFolder)
+    library.unzipFile(zipfile, xmlFolder)
 
     for xmlfile in sorted(os.listdir(xmlFolder)):
-        # get event id
         eqid = getEqid(f"{xmlFolder}/{xmlfile}")
         lastestEqid = getLatestEqid()
         print('Latest Event: ', lastestEqid, 'This Event: ', eqid)
         if eqid > lastestEqid:
-            originTImeUTC = getOriginTimeUTC(f"{xmlFolder}/{xmlfile}")
-            eqLongitude, eqLatitude, eqDepth, eqMagnitude = getEqParameters(f"{xmlFolder}/{xmlfile}")
-            df = getCwbStationInfo(f"{xmlFolder}/{xmlfile}")
-            # print(df)
-      
+            eqParameters = {}
+            library.getEqParameters(eqParameters, f"{xmlFolder}/{xmlfile}")
+            print(eqParameters)
+            df = library.getCwbStationInfo(f"{xmlFolder}/{xmlfile}")
+            print(df)
+     
 
-def removeExistOpendataApi():
-    global cwbOpendataApi
-    if os.path.isfile(cwbOpendataApi): 
-        os.remove(cwbOpendataApi)
-
-
-def downloadXMLfiles(filename):
-    global cwbOpendataApi
+def downloadXMLfiles(cwbOpendataApi, filename):
+    """
+    function downloadXMLfiles( cwbOpendataApi, filename )
+    
+    Author: Chaoyi Chen
+    Date: Fall 2021
+    
+    Module     : os
+                 wget
+              
+    Description: to download xmlfiles with eq information from CWB opendata api.
+                 
+    Parameters : cwbOpendataApi, str
+                 filename, str (output filename)
+    
+    Examples of sage:
+        >> downloadXMLfiles(cwbOpendataApi, zipfile)
+    """
     if os.path.isfile(filename):
         os.remove(filename)
     wget.download(f"https://opendata.cwb.gov.tw/{cwbOpendataApi}", out=filename)
 
 
 def getEqid(filename):
+    """
+    function getEqid( filename )
+    
+    Author: Chaoyi Chen
+    Date: Fall 2021
+    
+    Module     : ET (xml.etree.ElementTree)
+              
+    Description: to get the eq id number.
+                 
+    Parameters : filename, str (a xmlfile)
+    
+    Return     : eq id number, int
+    
+    Examples of sage:
+        >> xmlFolder = 'xmlfiles'
+        >> xmlfile = 'CWB-EQ110091-2021-0901-062756.xml'
+        >> print(getEqid(f"{xmlFolder}/{xmlfile}"))
+        110091
+    """
     tree = ET.parse(filename)
     root = tree.getroot()
-    return int(root[7][1][1].text)
+    ns = {'d': root.tag.split('}')[0].strip('{')}
+    eqid = root.find('d:dataset/d:earthquake/d:earthquakeNo', ns).text
+    return int(eqid)
 
 
 def getLatestEqid():
+    """
+    function getLatestEqid( )
+    
+    Author: Chaoyi Chen
+    Date: Fall 2021
+              
+    Description: to get the latest processed eq id number.
+    
+    Return     : latest eq id number, int
+    
+    Examples of sage:
+        >> print(getLatestEqid())
+        110091
+    """
     with open('latest.EQ') as f:
         return int(f.readlines()[0])
 
--- library.py
+++ library.py
@@ -4,71 +4,7 @@ import shutil
 import xml.etree.ElementTree as ET
 import zipfile
 from obspy.core import UTCDateTime
-    
-    
-def getEqParameters(xmlfile):
-    """
-    function getEqParameters( xmlfile )
-    
-    Author: Chaoyi Chen
-    Date: Fall 2021
-    
-    Module     : ET (xml.etree.ElementTree)
-            
-    Function   : ET.parse
-              
-    Description: to get the eq parameters (lat, lon dep, ML).
-                 
-    Parameters : xmlfile (a Xml file)
-    
-    Return     : eqLongitude, float
-                 eqLatitude, float 
-                 eqDepth, float 
-                 eqMagnitude, float
-    
-    Examples of sage:
-        >> xmlfile = 'CWB-EQ110095-2021-0915-185053.xml'
-        >> print(getEqParameters(xmlfile))
-        (121.39, 23.16, 20.6, 4.5)
-    """
-    tree = ET.parse(xmlfile)
-    root = tree.getroot()
-    eqLongitude = float(root[7][1][7][1][0].text)
-    eqLatitude = float(root[7][1][7][1][1].text)
-    eqDepth = float(root[7][1][7][2].text)
-    eqMagnitude = float(root[7][1][7][3][1].text)
-    return eqLongitude, eqLatitude, eqDepth, eqMagnitude
-
-
-def getOriginTimeUTC(xmlfile):
-    """
-    function getOriginTimeUTC( xmlfile )
-    
-    Author: Chaoyi Chen
-    Date: Fall 2021
-    
-    Module     : ET (xml.etree.ElementTree)
-                 obspy.core
-            
-    Function   : ET.parse
-                 obspy.core.UTCDateTime
-              
-    Description: to get the eq origin time from cwb xmlfile and 
-                 convert time zone from Taipei to UTC.
-                 
-    Parameters : xmlfile (a Xml file)
-    
-    Return     : the eq origin time as an UTCDateTime object
-    
-    Examples of sage:
-        >> xmlfile = 'libraryTest/CWB-EQ110095-2021-0915-185053.xml'
-        >> print(getOriginTimeUTC(xmlfile))
-        2021-09-15T10:50:53.000000Z
-    """
-    tree = ET.parse(xmlfile)
-    root = tree.getroot()
-    return UTCDateTime(root[7][1][7][0].text)
-
+        
 
 def unzipFile(inputName, outputName):
     """
@@ -101,10 +37,27 @@ def unzipFile(inputName, outputName):
         
         
 def getCwbStationInfo(xmlfile):
+    """
+    function getCwbStationInfo( xmlfile )
+    
+    Author: Chaoyi Chen
+    Date: Fall 2021
+    
+    Module     : ET (xml.etree.ElementTree)
+                 pd (pandas)
+              
+    Description: to get station information of a event.
+                 
+    Parameters : xmlfile, str (a xml file including event information)
+    
+    Return     : a pd.dataframe including all station information
+    
+    Examples of sage:
+        >> df = getCwbStationInfo(xmlfile)
+    """
     tree = ET.parse(xmlfile)
     root = tree.getroot()
-    # ns = {'d': root.tag.split('}')[0].strip('{')}
-    ns = {'d': 'urn:cwb:gov:tw:cwbcommon:0.1'}
+    ns = {'d': root.tag.split('}')[0].strip('{')}
     data = {'stationCode': [],
             'stationLon': [],
             'stationLat': [],
@@ -154,24 +107,73 @@ def getCwbStationInfo(xmlfile):
             data['stationPGAew'].append(float(station.find('d:pga/d:ewComponent', ns).text))
     df = pd.DataFrame(data)
     df['stationPGAmax'] = df[['stationPGAz', 'stationPGAns', 'stationPGAew']].max(axis=1)
-    print(df)
     return df
+
+
+def getEqParameters(dict, xmlfile):
+    """
+    function getEqParameters( xmlfile )
+    
+    Author: Chaoyi Chen
+    Date: Fall 2021
+    
+    Module     : ET (xml.etree.ElementTree)
+              
+    Description: to get the eq parameters (lat, lon dep, ML).
+                 
+    Parameters : dict, dict (an empty to output eq parameters)
+                 xmlfile, str (a xml file including event information)
+    
+    Examples of sage:
+        >> xmlfile = 'CWB-EQ110095-2021-0915-185053.xml'
+        >> eqParameters = {}
+        >> print(getEqParameters(eqParameters, xmlfile))
+        {'longitude': 121.39, 'latitude': 23.16, 'depth': 20.6, 'magnitude': 4.5}
+    """
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
+    ns = {'d': root.tag.split('}')[0].strip('{')}
+    eqParameters = root.find('d:dataset/d:earthquake/d:earthquakeInfo', ns)
+    dict['originTImeUTC'] = UTCDateTime(eqParameters.find('d:originTime', ns).text)
+    dict['longitude'] = float(eqParameters.find('d:epicenter/d:epicenterLon', ns).text)
+    dict['latitude'] = float(eqParameters.find('d:epicenter/d:epicenterLat', ns).text)
+    dict['depth'] = float(eqParameters.find('d:depth', ns).text)
+    dict['magnitude'] = float(eqParameters.find('d:magnitude/d:magnitudeValue', ns).text)
+
+
+def removeFile(filename):
+    """
+    function removeFile( filename )
+    
+    Author: Chaoyi Chen
+    Date: Fall 2021
+    
+    Module     : os
+              
+    Description: to remove a file.
+                 
+    Parameters : filename, str (file to remove)
+    
+    Examples of sage:
+        >> removeFile('text.txt')
+    """
+    if os.path.isfile(filename):
+        os.remove(filename)
         
 
 if __name__ == "__main__":
     print('You\'re in a function library.')
     
-    # print('\n---test getEqParameters---')
     xmlfile = 'libraryTest/CWB-EQ110091-2021-0901-062756.xml'
-    # print(getEqParameters(xmlfile))
-    # 
-    # print('\n---test getOriginTimeUTC---')
-    # print(getOriginTimeUTC(xmlfile))
+    # print('\n---test getEqParameters---')
+    # dict = {}
+    # getEqParameters(dict, xmlfile)
+    # print(dict)
     # 
     # print('\n---test unzipFile---')
     # inputName = 'libraryTest/download.zip'
     # outputName = 'libraryTest/unzipFolder'
     # unzipFile(inputName, outputName)
-    
-    print('\n---test getCwbStationInfo---')
-    getCwbStationInfo(xmlfile)
+    # 
+    # print('\n---test getCwbStationInfo---')
+    # print(getCwbStationInfo(xmlfile))
